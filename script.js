17306a025198f6d6d0c8062e937a34e68502191e
YanDamasco <yandamascoprofissional@gmail.com>
2025-07-22 12:02:27 -0300

    animate.css add on the project. Create some sections like About Us

--- script.js
+++ script.js
@@ -2,3 +2,14 @@ window.addEventListener("scroll", function () {
     let header = this.document.querySelector("#header")
     header.classList.toggle("scrolling", this.window.scrollY > 50)
 });
+
+const aboutTitle = document.getElementById("aboutTitle");
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            aboutTitle.classList.add("animate__animated", "animate__fadeInDown");
+        }
+    });
+}, { threshold: 0.5 });
+
+observer.observe(aboutTitle);
